631a09c0a9c93f7d4247550135a75b313353578e
Amlan Sarmah <amlansarmah26@gmail.com>
2024-11-18 00:34:05 +0530

    remove middleware, critical

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(request: Request) {
-  // Set the timezone to IST (Asia/Kolkata)
-  process.env.TZ = "Asia/Kolkata";
-
-  return NextResponse.next();
-}
